4aba2bea2be3d381a9638db3d219659553e2d2ee
herrkj <nikoarbeit@aol.de>
2026-02-19 15:40:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -598,4 +598,6 @@ def export_csv():
 
 if __name__ == "__main__":
     # локальный запуск: http://127.0.0.1:5000
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
